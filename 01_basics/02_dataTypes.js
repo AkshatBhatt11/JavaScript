cd689698e6c68d8e5e2f9d78a4569d98d01ab7ca
Akshat Bhatt <142761449+AkshatBhatt11@users.noreply.github.com>
2024-06-17 15:21:58 +0000

    datatypes in JS

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //to declare that below js file is in newer version
+
+//alert() is not use for output in nodejs
+
+console.log(3+3); console.log("ak") //this is not readable code
+
+let name = "vk"
+let age = 21
+let isLogIn = true
+
+// int number range 2 power 53
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value(empty)
+// undefined = value not assign
+// symbol = unique value
+
+// object
+
+console.log(typeof null); // it gives output object
+console.log(typeof undefined); // it gives output undefined
