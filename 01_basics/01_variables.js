aebe95579fe076a197d703fd1192708667f0553a
Akshat Bhatt <142761449+AkshatBhatt11@users.noreply.github.com>
2024-06-17 20:11:03 +0530

    variables in javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accId = 11
+let accMail = "ak@gmail.cm"
+var accPass = "aaa123"
+accName = "Akshat"
+let accType = ""
+
+console.log(accName)
+/*
+Let and Var are both same but never use var because of global scope and functional scope
+*/
+
+console.table([accId, accMail, accPass, accName, accType])
