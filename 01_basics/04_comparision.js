18cc4d9b6854f6cd0b2f6845ba94063747041b06
Akshat Bhatt <142761449+AkshatBhatt11@users.noreply.github.com>
2024-06-18 11:24:58 +0000

    comparision in javascript

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// Normal Comparision output in true or false
+console.log(2 > 1)
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log(2 == 1)
+console.log(2 != 1)
+
+// two different datatype comparision output in true or false
+console.log("2" > 1)
+console.log("02" > 1)
+
+// comparision with null output in true or false
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+// compare with undefined output always false
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+// == compare value and === comapre value and datatype both
+console.log("2" === 2)
