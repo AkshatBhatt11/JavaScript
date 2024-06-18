18558ed62b11572b43a933de9761cdb7d00958e2
Akshat Bhatt <142761449+AkshatBhatt11@users.noreply.github.com>
2024-06-18 11:08:29 +0000

    Operation in javascript

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,43 @@ let someNumber = "14"
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+// *********** Operation ***********
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+//Normal operation
+console.log(2+2)
+console.log(2-3)
+console.log(2*5)
+console.log(2**4)
+console.log(4/2)
+console.log(90%3)
+
+//Add two string
+let str1 = "hello "
+let str2 = "akshat"
+
+let str3 = str1 + str2
+console.log(str3)
+
+//Complex Operation
+console.log("1" + 2) // output 12
+console.log(1 + "2") // output 12
+console.log("1" + 2 + 2) // output 122
+console.log(1 + 2 + "2") // output 32
+
+console.log( 3 + 4 * 5 / 6) // not readable code 
+
+console.log(+true) // output 1
+console.log(+"") // output 0 
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 // not readable code
+
+let gameCounter = 10
+gameCounter++
+console.log(gameCounter)// prefix and postfix
