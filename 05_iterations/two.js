92eb4633bd1416489c57aed8bb047f1391e8284e
Akshat Bhatt <142761449+AkshatBhatt11@users.noreply.github.com>
2024-07-04 04:59:34 +0000

    while and do-while loop in javascript

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+// While loop and Do-While
+
+let i = 0
+while (i <= 10) {
+    //console.log(`Value of i is ${i}`);
+    i = i + 2;
+}
+
+let myArr = ["ak", "vk", "mk"]
+let arr = 0
+while (arr < myArr.length) {
+    //console.log(`My Array is ${myArr[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
